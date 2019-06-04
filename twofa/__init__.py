60e766373fcc3acfd9911ff528dc64e7e8c843c4
pepa65 <pepa65@passchier.net>
2019-06-04 11:36:24 -0700

    Robustisation

--- twofa/__init__.py
+++ twofa/__init__.py
@@ -16,12 +16,12 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-version='0.11'
+version = '0.12'
+configfile = '~/.twofa.yaml'
 
 def create_fernet(passwd, salt=None):
 	if salt is None:
 		salt = os.urandom(16)
-
 	kdf = PBKDF2HMAC(
 		algorithm=hashes.SHA256(),
 		length=32,
@@ -46,20 +46,16 @@ class Store(object):
 	passwd = None
 	def load_secrets(self):
 		try:
-			with open(os.path.expanduser('~/.twofa.yaml'), 'r') as outfile:
+			with open(os.path.expanduser(configfile), 'r') as outfile:
 				data = yaml.load(outfile)
 			try:
 				if data['encrypted']:
 					self.passwd = getpass.getpass("Enter store password: ")
 					self.encrypted = True
 					try:
-						data['secrets'] = decrypt(
-							data['secrets'], self.passwd, data['salt']
-						)
+						data['secrets'] = decrypt(data['secrets'], self.passwd, data['salt'])
 					except InvalidToken:
-						raise click.ClickException("Invalid password")
-
-				click.echo("OK\n")
+						raise click.ClickException("invalid password")
 				return data['secrets']
 			except KeyError:
 				return data
@@ -71,20 +67,16 @@ class Store(object):
 		if passwd is not None:
 			self.passwd = passwd
 			self.encrypted = True
-
 		if passwd == "":
 			self.encrypted = False
-
 		if self.encrypted:
 			secrets, salt = encrypt(secrets, self.passwd)
-
 		data = {
 			'encrypted': self.encrypted,
 			'salt': salt,
 			'secrets': secrets
 		}
-
-		with open(os.path.expanduser('~/.twofa.yaml'), 'w') as outfile:
+		with open(os.path.expanduser(configfile), 'w') as outfile:
 			yaml.dump(data, outfile, default_flow_style=False)
 
 def totp(secret):
@@ -94,123 +86,118 @@ def totp(secret):
 @click.group(invoke_without_command=True)
 @click.pass_context
 def cli(ctx):
-	""" twofa - Manage two-factor authentication store """
+	"""twofa - Manage two-factor authentication store"""
 	if ctx.invoked_subcommand is None:
 		return listcmd()
 
 @cli.command(name='list')
 @click.argument('pattern', default="")
 def listcmd(pattern):
-	""" List secrets that match regex pattern """
+	"""List secrets that match regex pattern"""
 	store = Store()
 	secrets = store.load_secrets()
 	some = False
 	for label, secret in secrets.items():
-		#if pattern == "" or re.match(pattern+"$", label):
 		if pattern == "" or re.search(pattern, label, re.IGNORECASE):
 			click.echo("{}  {}".format(totp(secret), label))
 			some = True
-
 	if some:
 		expire = 30-(int(time.time())%30)
-		click.echo("\nExpiration in {} seconds".format(expire))
+		click.echo("Expiration in {} seconds".format(expire))
 	else:
-		click.echo("Nothing found matching pattern '{}'".format(pattern))
+		click.echo("No match for '{}'".format(pattern))
 
 @cli.command(name='add')
 @click.argument('label')
-@click.argument('secret')
-def addcmd(label, secret):
-	""" Add secret to store """
+def addcmd(label):
+	"""Add secret to store"""
 	store = Store()
 	secrets = store.load_secrets()
 	if secrets.get(label):
-		raise click.ClickException("Label '{}' already present, aborting".format(label))
-
-	try:
-		secret = "".join(secret.split())
-		totp(secret)
-		secrets[label] = secret
-	except (TypeError, binascii.Error):
-		raise click.ClickException("Invalid secret! Only A-Z and 2-7 allowed, and no spaces, aborting")
-
-	store.save_secrets(secrets)
-	click.echo("Secret stored with label '{}'".format(label))
+		raise click.ClickException("label '{}' already present".format(label))
+	while True:
+		secret = input("Enter secret (only A-Z and 2-7 allowed): ")
+		if secret == "": break
+		try:
+			secret = "".join(secret.strip().split())
+			totp(secret)
+			secrets[label] = secret
+			break
+		except (TypeError, binascii.Error):
+			raise click.ClickException("invalid secret")
+			continue
+	if secret != "":
+		store.save_secrets(secrets)
+		click.echo("Secret {} stored with label '{}'".format(secret, label))
 
 @cli.command(name='rename')
 @click.argument('label')
 @click.argument('new_label')
 def renamecmd(label, new_label):
-	""" Rename secret in store """
+	"""Rename secret in store"""
 	store = Store()
 	secrets = store.load_secrets()
-
-	try:
-		secrets[new_label] = secrets[label]
-		del secrets[label]
-	except KeyError:
-		raise click.ClickException("Label '{}' not present, aborting".format(label))
-
-	store.save_secrets(secrets)
-	click.echo("Label '{}' renamed to '{}'".format(label, new_label))
+	if secrets[new_label]:
+		click.echo("No: label '{}' already exists".format(new_label))
+	else:
+		try:
+			secrets[new_label] = secrets[label]
+			del secrets[label]
+		except KeyError:
+			raise click.ClickException("label '{}' does not exist".format(label))
+		store.save_secrets(secrets)
+		click.echo("Label '{}' renamed to '{}'".format(label, new_label))
 
 @cli.command(name='remove')
 @click.argument('label')
-@click.option('--confirm/--no-confirm', default=False,
+@click.option('--confirm', is_flag=True,
+		prompt="Are you sure you want to remove this label?",
 		help="Option --confirm is required for remove")
 def rmcmd(label, confirm):
-	""" Remove secret: --confirm option required! """
+	"""Remove secret: --confirm option required!"""
 	store = Store()
 	secrets = store.load_secrets()
-
 	try:
 		if secrets[label]: pass
 	except KeyError:
-		raise click.ClickException("Label '{}' not present, aborting".format(label))
-
+		raise click.ClickException("label '{}' does not exist".format(label))
 	if not confirm:
-		raise click.ClickException("The --confirm option is required, aborting")
+		raise click.ClickException("--confirm option is required")
 	try:
 		del secrets[label]
 	except KeyError:
-		raise click.ClickException("Label '{}' not present, aborting".format(label))
-
+		raise click.ClickException("label '{}' does not exist".format(label))
 	store.save_secrets(secrets)
-	click.echo("Label '{}' removed with secret".format(label))
+	click.echo("Secret and label '{}' removed".format(label))
 
 @cli.command(name='qr')
 @click.argument('label')
-@click.option('--invert/--no-invert', default=False,
-		help="Black on white, or white on black")
+@click.option('--invert', '-i', is_flag=True,
+		help="White on black instead")
 def qrcmd(label, invert):
-	""" Print QR code for secret """
+	"""Print QR code for secret"""
 	store = Store()
 	secrets = store.load_secrets()
 	secret = secrets.get(label)
-
 	if secret:
-		qr = pyqrcode.create(
-			'otpauth://totp/{}?secret={}'.format(label, secret.upper())
-		)
+		qr = pyqrcode.create('otpauth://totp/{}?secret={}'.format(label, secret.upper()))
 		if invert:
-			click.echo(qr.terminal(
-				module_color='black', background='white', quiet_zone=1
-			))
+			click.echo(qr.terminal(module_color='white', background='black',
+					quiet_zone=1))
 		else:
-			click.echo(qr.terminal(quiet_zone=1))
+			click.echo(qr.terminal(module_color='black', background='white',
+					quiet_zone=1))
 	else:
-		raise click.ClickException("Label '{}' not present".format(label))
+		raise click.ClickException("label '{}' does not exist".format(label))
 
 @cli.command(name='password')
 def passwdcmd():
-	""" Set password: empty = no encryption! """
+	"""Set password (empty: no encryption!)"""
 	store = Store()
 	secrets = store.load_secrets()
-
 	newpasswd = getpass.getpass("Enter new store password: ")
 	confirmpasswd = getpass.getpass("Confirm new store password: ")
-
 	if not newpasswd == confirmpasswd:
-		raise click.ClickException("New store passwords not matching, aborting")
+		raise click.ClickException("passwords not matching")
 	store.save_secrets(secrets, newpasswd)
 	click.echo("New password set")
