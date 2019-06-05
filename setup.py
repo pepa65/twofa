9eec4843fd50f9cce43e901b0b1b2fc5773d4789
pepa65 <pepa65@passchier.net>
2019-06-05 01:05:03 -0700

    Version 0.22 Reformat output

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ import setuptools
 
 setuptools.setup(
     name='twofa',
-    version='0.21',
+    version='0.22',
     description='A simple command-line 2-factor authentication token manager',
-    author='Nils Werner',
+    author='Nils Werner, pepa65',
+    author_email='pepa65@passchier.net',
+    licence='GPLv3+',
     long_description='',
-    url='https://github.com/nils-werner/twofa',
+    url='https://github.com/pepa65/twofa',
     entry_points={'console_scripts': ['twofa = twofa.__init__:cli']},
     install_requires=[
         'pyqrcode',
--- twofa/__init__.py
+++ twofa/__init__.py
@@ -1,13 +1,11 @@
 from __future__ import print_function, absolute_import
 import os
-import sys
 import time
 import base64
 import getpass
-import binascii
-
 import re
 import click
+import binascii
 import pyotp
 import pyqrcode
 import yaml
@@ -16,7 +14,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-version = '0.12'
+version = '0.22'
 configfile = '~/.twofa.yaml'
 
 def create_fernet(passwd, salt=None):
@@ -26,7 +24,7 @@ def create_fernet(passwd, salt=None):
 		algorithm=hashes.SHA256(),
 		length=32,
 		salt=salt,
-		iterations=500000,
+		iterations=777777,
 		backend=default_backend()
 	)
 	key = base64.urlsafe_b64encode(kdf.derive(passwd.encode()))
@@ -96,14 +94,14 @@ def listcmd(pattern):
 	"""List secrets that match regex pattern"""
 	store = Store()
 	secrets = store.load_secrets()
-	some = False
+	list = ""
+	expire = 30-(int(time.time())%30)
 	for label, secret in secrets.items():
 		if pattern == "" or re.search(pattern, label, re.IGNORECASE):
-			click.echo("{}  {}".format(totp(secret), label))
-			some = True
-	if some:
-		expire = 30-(int(time.time())%30)
-		click.echo("Expiration in {} seconds".format(expire))
+			list += "{}    {:2d} s    {}\n".format(totp(secret), expire, label)
+	if list != "":
+		header = " TOTP    Expire    Label\n".format(expire)
+		click.echo_via_pager(header+list)
 	else:
 		click.echo("No match for '{}'".format(pattern))
 
@@ -114,21 +112,21 @@ def addcmd(label):
 	store = Store()
 	secrets = store.load_secrets()
 	if secrets.get(label):
-		raise click.ClickException("label '{}' already present".format(label))
-	while True:
-		secret = input("Enter secret (only A-Z and 2-7 allowed): ")
-		if secret == "": break
-		try:
-			secret = "".join(secret.strip().split())
-			totp(secret)
-			secrets[label] = secret
-			break
-		except (TypeError, binascii.Error):
-			raise click.ClickException("invalid secret")
-			continue
-	if secret != "":
-		store.save_secrets(secrets)
-		click.echo("Secret {} stored with label '{}'".format(secret, label))
+		raise click.ClickException("label '{}' already exists".format(label))
+	else:
+		while True:
+			secret = input("Enter secret (only A-Z and 2-7 allowed): ")
+			if secret == "": break
+			try:
+				secret = "".join(secret.strip().split())
+				totp(secret)
+				secrets[label] = secret
+				break
+			except (TypeError, binascii.Error):
+				click.echo("No: invalid base32 secret")
+		if secret != "":
+			store.save_secrets(secrets)
+			click.echo("Secret {} stored with label '{}'".format(secret, label))
 
 @cli.command(name='rename')
 @click.argument('label')
@@ -137,16 +135,18 @@ def renamecmd(label, new_label):
 	"""Rename secret in store"""
 	store = Store()
 	secrets = store.load_secrets()
-	if secrets[new_label]:
-		click.echo("No: label '{}' already exists".format(new_label))
+	try: secrets[label]
+	except KeyError:
+		raise click.ClickException("label '{}' doesn't exist".format(label))
 	else:
-		try:
+		try: secrets[new_label]
+		except:
 			secrets[new_label] = secrets[label]
 			del secrets[label]
-		except KeyError:
-			raise click.ClickException("label '{}' does not exist".format(label))
-		store.save_secrets(secrets)
-		click.echo("Label '{}' renamed to '{}'".format(label, new_label))
+			store.save_secrets(secrets)
+			click.echo("Label '{}' renamed to '{}'".format(label, new_label))
+		else:
+			raise click.ClickException("label '{}' already exists".format(new_label))
 
 @cli.command(name='remove')
 @click.argument('label')
@@ -157,18 +157,14 @@ def rmcmd(label, confirm):
 	"""Remove secret: --confirm option required!"""
 	store = Store()
 	secrets = store.load_secrets()
-	try:
-		if secrets[label]: pass
-	except KeyError:
+	if not secrets[label]:
 		raise click.ClickException("label '{}' does not exist".format(label))
-	if not confirm:
+	elif not confirm:
 		raise click.ClickException("--confirm option is required")
-	try:
+	else:
 		del secrets[label]
-	except KeyError:
-		raise click.ClickException("label '{}' does not exist".format(label))
-	store.save_secrets(secrets)
-	click.echo("Secret and label '{}' removed".format(label))
+		store.save_secrets(secrets)
+		click.echo("Secret and label '{}' removed".format(label))
 
 @cli.command(name='qr')
 @click.argument('label')
